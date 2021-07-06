f107b16588f7c927084ee2b0f295160ce986d23a
janki <jankikumar2012@gmail.com>
2021-07-06 17:48:29 +0530

    first commit

---
+++ school management/src/main/java/com/school/model/Student.java
@@ -0,0 +1,33 @@
+package com.school.model;
+
+public class Student {
+
+	private String name;
+	private int roll;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
